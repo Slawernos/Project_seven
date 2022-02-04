dd20daf1685878055b082e58ea678bc7af534601
Slawernos <79807306+Slawernos@users.noreply.github.com>
2022-02-04 17:44:36 +0000

    Add files via upload

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,6 +7,7 @@ const multer = require('../middleware/multer')
 router.get('/', auth.auth, postsController.getAll);
 router.get('/getchunk', auth.auth, postsController.getChunk);
 router.get('/getone', auth.auth, postsController.getOne);
+router.get('/weekly', auth.auth, postsController.weekly);
 router.delete('/', auth.auth, postsController.deletePost);
 router.put('/', auth.auth, multer, postsController.updatePost)
 router.post('/', auth.auth, multer, postsController.addPost,);
